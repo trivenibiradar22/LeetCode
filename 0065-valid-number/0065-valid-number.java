65a1537c25def10c81518f8bce37915cad64d371
trivenibiradar22 <202401070172@mitaoe.ac.in>
2026-07-19 22:18:40 +0530

    Time: 1 ms (100%), Space: 43.9 MB (68.35%) - LeetHub

---
+++ 0065-valid-number/0065-valid-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isNumber(String s) {
+        boolean num = false, dot = false, exp = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                num = true;
+            } else if (c == '.') {
+                if (dot || exp) return false;
+                dot = true;
+            } else if (c == 'e' || c == 'E') {
+                if (exp || !num) return false;
+                exp = true;
+                num = false;
+            } else if (c == '+' || c == '-') {
+                if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E')
+                    return false;
+            } else {
+                return false;
+            }
+        }
+
+        return num;
+    }
+}
