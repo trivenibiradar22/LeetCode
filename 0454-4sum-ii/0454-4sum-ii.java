1cf87e3127744275d01e7c549b9bfeebabd350e0
trivenibiradar22 <202401070172@mitaoe.ac.in>
2026-08-02 20:15:24 +0530

    Time: 117 ms (40.31%), Space: 47.1 MB (16.47%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2,
+                            int[] nums3, int[] nums4) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int a : nums1) {
+            for (int b : nums2) {
+                int sum = a + b;
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+
+        int count = 0;
+
+        for (int c : nums3) {
+            for (int d : nums4) {
+                int sum = c + d;
+                count += map.getOrDefault(-sum, 0);
+            }
+        }
+
+        return count;
+    }
+}
