76cf21c0fab8c848ac6d947f789733e16c964daa
trivenibiradar22 <202401070172@mitaoe.ac.in>
2026-08-13 18:10:57 +0530

    Time: 15 ms (59.58%), Space: 51.4 MB (74.93%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -3,15 +3,20 @@ class Solution {
         Arrays.sort(g);
         Arrays.sort(s);
 
-        int i = 0, j = 0;
+        int i = 0;
+        int j = 0;
+        int count = 0;
 
         while (i < g.length && j < s.length) {
             if (s[j] >= g[i]) {
+                count++;
                 i++;
+                j++;
+            } else {
+                j++;
             }
-            j++;
         }
 
-        return i;
+        return count;
     }
 }
