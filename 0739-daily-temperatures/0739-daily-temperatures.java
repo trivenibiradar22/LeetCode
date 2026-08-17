906395b96fe3027be8a05e659dcbd7a31f488947
trivenibiradar22 <202401070172@mitaoe.ac.in>
2026-08-17 18:22:05 +0530

    Time: 77 ms (26.15%), Space: 106.9 MB (50.64%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int n = temperatures.length;
+        int[] answer = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!stack.isEmpty() &&
+                   temperatures[stack.peek()] <= temperatures[i]) {
+                stack.pop();
+            }
+
+            if (!stack.isEmpty()) {
+                answer[i] = stack.peek() - i;
+            }
+
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
