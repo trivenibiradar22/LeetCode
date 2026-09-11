f0454c8523f54a109218f0726ad5f29c3d888217
trivenibiradar22 <202401070172@mitaoe.ac.in>
2026-09-11 15:57:33 +0530

    Time: 7 ms (20.3%), Space: 50.2 MB (14.47%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,31 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<String> restoreIpAddresses(String s) {
-        List<String> ans = new ArrayList<>();
-        backtrack(s, 0, 0, "", ans);
-        return ans;
+        List<String> result = new ArrayList<>();
+        backtrack(s, 0, 0, "", result);
+        return result;
     }
 
-    private void backtrack(String s, int index, int parts, String path, List<String> ans) {
-        if (parts == 4 && index == s.length()) {
-            ans.add(path.substring(0, path.length() - 1));
+    private void backtrack(String s, int startIndex, int dotCount, String currentIp, List<String> result) {
+        if (dotCount == 4 && startIndex == s.length()) {
+            result.add(currentIp.substring(0, currentIp.length() - 1));
+            return;
+        }
+        if (dotCount == 4 || startIndex == s.length()) {
             return;
         }
 
-        if (parts == 4 || index == s.length()) {
-            return;
-        }
-
-        for (int len = 1; len <= 3 && index + len <= s.length(); len++) {
-            String part = s.substring(index, index + len);
-
-            if (part.length() > 1 && part.charAt(0) == '0') {
-                break;
-            }
-
-            int num = Integer.parseInt(part);
-            if (num <= 255) {
-                backtrack(s, index + len, parts + 1, path + part + ".", ans);
+        for (int i = startIndex; i < Math.min(startIndex + 3, s.length()); i++) {
+            String part = s.substring(startIndex, i + 1);
+            if (isValid(part)) {
+                backtrack(s, i + 1, dotCount + 1, currentIp + part + ".", result);
             }
         }
+    }
+
+    private boolean isValid(String part) {
+        if (part.length() > 1 && part.startsWith("0")) {
+            return false;
+        }
+        int val = Integer.parseInt(part);
+        return val >= 0 && val <= 255;
     }
 }
