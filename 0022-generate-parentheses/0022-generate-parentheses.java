dfc46d63e16634c32bb8e8188b3c2e5323c78eae
trivenibiradar22 <202401070172@mitaoe.ac.in>
2026-09-19 19:01:32 +0530

    Time: 0 ms (100%), Space: 44.5 MB (64.19%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,27 +1,26 @@
-import java.util.*;
-
 class Solution {
-
-    List<String> result = new ArrayList<>();
-
     public List<String> generateParenthesis(int n) {
-        backtrack("", 0, 0, n);
+        List<String> result = new ArrayList<>();
+        backtrack(result, new StringBuilder(), 0, 0, n);
         return result;
     }
 
-    private void backtrack(String current, int open, int close, int n) {
-
-        if (current.length() == 2 * n) {
-            result.add(current);
+    private void backtrack(List<String> result, StringBuilder current, int open, int close, int max) {
+        if (current.length() == max * 2) {
+            result.add(current.toString());
             return;
         }
 
-        if (open < n) {
-            backtrack(current + "(", open + 1, close, n);
+        if (open < max) {
+            current.append("(");
+            backtrack(result, current, open + 1, close, max);
+            current.deleteCharAt(current.length() - 1);
         }
 
         if (close < open) {
-            backtrack(current + ")", open, close + 1, n);
+            current.append(")");
+            backtrack(result, current, open, close + 1, max);
+            current.deleteCharAt(current.length() - 1);
         }
     }
 }
